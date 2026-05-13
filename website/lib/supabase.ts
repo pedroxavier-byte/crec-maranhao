1a71ebca41033110769c937950be5fdc4d4455dc
Pedro Xavier <pedro.xavier@ufma.br>
2026-05-13 16:27:21 -0300

    Corrigir chave do Supabase

--- website/lib/supabase.ts
+++ website/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://pqpwhekpuyptpweyeymh.supabase.co';
-const supabaseKey = 'sb_publishable_dQOYcOS1x_M1YX1i4vERVg_Y_djA1VN';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxcHdoZWtwdXlwdHB3ZXlleW1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODY2ODYsImV4cCI6MjA5NDI2MjY4Nn0.FSiLo44CY6h8MjH7CQ7oWH1dgvEo9HNoVx5w0LvXSJM';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
